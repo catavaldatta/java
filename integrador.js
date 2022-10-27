5cb7c600ab7a9a3d41dbcd38d68b4d16f85871b9
Catarina Valdatta <catarina.valdatta@gmail.com>
2022-10-26 21:05:20 -0300

    come

--- integrador.js
+++ integrador.js
@@ -8,36 +8,61 @@ function validarUsu() {
     if (correo.length==0) {
         console.log ("usu vacio")
         document.getElementById("msjUsu").innerHTML="El usuario no puede estar vacío"
-
+        document.getElementById("msjUsu").style="color: red"
     } else if(correo.indexOf("@")==-1){
         console.log("usu No vacío, sin @")
         document.getElementById("msjUsu").innerHTML="El usuario debe contener un @"
     } else if(correo.indexOf(".com")==-1){
         console.log("usu No vacío, sin .com")
         document.getElementById("msjUsu").innerHTML="El usuario debe contener el dominio '.com'"
+    } else {
+        console.log("usu correcto")
+        document.getElementById("msjUsu").innerHTML=""
     }
 }
 
 
+const ticket=200
+const estudiante=80
+const trainee=50
+const junior=15
 
-function total(){
-    console.log ("Ingreso a total")
+function Total() {
     let cantidad=document.getElementById("cantidad").value
-    console.log(cantidad)
-    let categoria=document.getElementById("categoria").innerHTML
+    
+    let categoria=document.getElementById("categoria").value
 
-    if (categoria="Elige una opción"){
-        document.getElementById("monto").innerHTML= 200*cantidad
-    } else if (categoria="Estudiante"){
-        document.getElementById("monto").innerHTML= (200*cantidad)*0.8
+    let total=0
+    let subtotal=cantidad*ticket
+    if (categoria=="Elige una opción") {
+        total=cantidad*ticket
+    } else if (categoria=="Estudiante") {
+        total=subtotal-(cantidad*ticket*0.8)
+    } else if (categoria=="Trainee") {
+        total=subtotal-(cantidad*ticket*0.5)
+    } else {
+        total=subtotal-(cantidad*ticket*0.15) 
     }
-    else if (categoria="Trainee"){
-        document.getElementById("monto").innerHTML= (200*cantidad)*0.5
-    }
-    else if (categoria="Junior"){
-        document.getElementById("monto").innerHTML= (200*cantidad)*0.15
-    }
+    document.getElementById("monto").innerHTML=`Total a pagar: $${total}`
+    
 }
 
 
+function borrar(){
 
+    document.getElementById("nombre").value = ""
+    document.getElementById("apellido").value = ""
+    document.getElementById("correo").value = ""
+    document.getElementById("cantidad").value = ""    
+    document.getElementById("categoria").value =""
+    document.getElementById("monto").innerHTML="Total a Pagar: $ "
+}
+
+
+var input = document.getElementById("info");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("enter").click();
+  }
+});
